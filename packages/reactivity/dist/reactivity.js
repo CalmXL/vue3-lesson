d13d2d31f51ea93e27e10675136af36cccd7bc75
xulei <xl18845898715@gmail.com>
2024-06-26 17:35:56 +0800

    feat: reactivity 创建基本目录结构

--- packages/reactivity/dist/reactivity.js
+++ packages/reactivity/dist/reactivity.js
@@ -1,8 +1,34 @@
+// packages/reactivity/src/effect.ts
+function effect() {
+}
+
 // packages/shared/src/index.ts
 function isObject(value) {
   return typeof value === "object" && value !== null;
 }
 
-// packages/reactivity/src/index.ts
-console.log(isObject);
+// packages/reactivity/src/reactive.ts
+var reactiveMap = /* @__PURE__ */ new WeakMap();
+var mutableHandlers = {
+  get(target, key, receiver) {
+  },
+  set(target, key, value, receiver) {
+    return false;
+  }
+};
+function reactive(target) {
+  return createReactiveObject(target);
+}
+function createReactiveObject(target) {
+  if (!isObject(target)) return;
+  const existProxy = reactiveMap.get(target);
+  if (existProxy) return existProxy;
+  let proxy = new Proxy(target, mutableHandlers);
+  reactiveMap.set(target, proxy);
+  return proxy;
+}
+export {
+  effect,
+  reactive
+};
 //# sourceMappingURL=reactivity.js.map
